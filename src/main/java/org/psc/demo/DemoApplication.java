4b840d85c6ae99a8738724634d3a2538bb5bdf10
sykq <genericmailacc@gmail.com>
2019-08-20 21:10:52 +0200

    gitignore

---
+++ src/main/java/org/psc/demo/DemoApplication.java
@@ -0,0 +1,11 @@
+package org.psc.demo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        log.info("DemoApplication started");
+    }
+}
