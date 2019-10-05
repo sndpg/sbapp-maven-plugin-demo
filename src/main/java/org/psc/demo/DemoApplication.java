18ff0e87aad883f1987f60cabc7492fa1fc809a0
sykq <genericmailacc@gmail.com>
2019-10-05 10:55:44 +0200

    run SpringBootApplication from artifact which uses the plugin

--- src/main/java/org/psc/demo/DemoApplication.java
+++ src/main/java/org/psc/demo/DemoApplication.java
@@ -1,11 +1,23 @@
 package org.psc.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.EventListener;
 
 @Slf4j
+@SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void applicationReady() {
         log.info("DemoApplication started");
+        SpringApplication.exit(applicationContext);
     }
 }
